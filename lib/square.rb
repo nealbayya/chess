ce225272c20e3659d693cd5c65f403a62f58cb35
Neal Bayya <neal.bayya@gmail.com>
2020-07-10 14:43:29 -0400

    added piece detatchment/placement to square functionality

--- lib/square.rb
+++ lib/square.rb
@@ -1,6 +1,6 @@
 require_all 'pieces/'
 class Square
-  attr_accessor :row_index, :col_index, :occupied, :piece, :light
+  attr_reader :row_index, :col_index, :occupied, :piece, :light
   def initialize(row_index, col_index, light)
     @row_index = row_index 
     @col_index = col_index
@@ -8,4 +8,23 @@ class Square
     @piece = nil
     @light = light
   end
+
+  def detatch_piece
+    if @occupied
+      @occupied = false
+      @detatched_piece = @piece
+      @piece = nil 
+      return @detatched_piece
+    end
+    return nil
+  end
+
+  def place_piece(piece)
+    if @occupied
+      raise "Square is not empty"
+    end
+    @occupied = true
+    @piece = Piece
+  end
+
 end
